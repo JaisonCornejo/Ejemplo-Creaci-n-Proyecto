90dda61997238576afc91247b825bb70eb4982dc
JaisonCornejo <66445503+JaisonCornejo@users.noreply.github.com>
2020-08-20 22:45:47 -0400

    Codigo Entrada analoga Salida Serie

---
+++ EASS.ino
@@ -0,0 +1,30 @@
+int sensorValue = 0;//Se asigna valor cero a la variable sensorValue además de definir que es 0.
+
+int outputValue = 0;//Se asigna valor cero a la variable outputValue además de definir que es 0
+
+void setup()
+{
+  pinMode(A0, INPUT);//• El pin A0 es entrada
+  pinMode(9, OUTPUT);//• El pin 9 es salida 
+  Serial.begin(9600);//• Se establece la velocidad de datos en 9600 bits por segundo.
+}
+void loop()
+{
+
+  sensorValue = analogRead(A0);//• Leer el potenciómetro a través del pin A0 de manera análoga.
+ 
+  outputValue = map(sensorValue, 0, 1023, 0, 255);//•  Se asigna el rango de la salida analógica es decir 0V-0 y 1023 -255.
+
+  analogWrite(9, outputValue);//•  Se define la salida el pin 9 a través de la lectura del potenciómetro.
+
+                          //•  Se imprimen en pantalla los resultados:
+
+
+Serial.print("sensor = ");
+Serial.print(sensorValue);//-Sensor: Valor de lectura del análogo potenciómetro (valor entre 0-1023)
+
+  Serial.print("\t output = ");
+  Serial.println(outputValue);//-Salida: Valor de salida digital. 
+
+  delay(2); //Se espera 2 milisegundos antes del siguiente ciclo para convertir el valor  de analógico al digital para que el ciclo se vuelva a ejecutar.
+}
